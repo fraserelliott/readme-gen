ff3c702c8660ac407678ac23de24061574f12fed
Fraser Elliott <fraserelliott@hotmail.com>
2025-07-06 21:10:31 +0100

    started logic for parsing template file

--- readme_generator.py
+++ readme_generator.py
@@ -5,6 +5,7 @@ class ReadmeGenerator:
     def __init__(self, settings):
         self.settings = settings
         self.sections = list()
+        self.header = ""
 
     def add_section(self, section):
         self.sections.append(section)
@@ -13,6 +14,21 @@ class ReadmeGenerator:
         console = Console()
         for section in self.sections:
             section.prompt(console)
+            
+    def parse_template(self, template_path):
+        currentSection = None
+        with open(template_path) as file:
+            for line in file:
+                if line[0] == "#":
+                    section_title = line.lstrip("#").strip() #remove the starting # and any whitespace between to get the title
+                    currentSection = Section(section_title, "") #Creating with "" as we add line later
+                    self.add_section(currentSection)
+            
+                if currentSection:
+                    currentSection.text += line
+                else:
+                    self.header += line #if we haven't started a section yet, then append to header
+                    
         
     def default(self): #test until I have the template reader made
         #Project title

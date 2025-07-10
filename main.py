b8c79e2bcf2aaf07165c147c6159783bd93b8694
Fraser Elliott <fraserelliott@hotmail.com>
2025-07-10 20:02:52 +0100

    refactored main.py to use a class MainMenu which handles routing

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import argparse
+import os
 from readme_generator_textual import ReadmeGenerator
 from settings import Settings
 
-settings = Settings()
-settings.load("test-files/settings.json")
+SETTINGS_LOCATION = "test-files/settings.json"
+class MainMenu:
+    def __init__(self):
+        # Arg parsing and routing with options for -h or --help
+        self.parser = argparse.ArgumentParser()
+        group = self.parser.add_mutually_exclusive_group()
+        group.add_argument("--settings", action="store_true", help="Open settings configuration")
+        group.add_argument("--template", action="store_true", help="Open template wizard")
+        group.add_argument("--regenerate", action="store_true", help="Open regeneration wizard")
 
-# Arg parsing and routing with options for -h or --help
-parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group()
-group.add_argument("--settings", action="store_true", help="Open settings configuration")
-group.add_argument("--template", action="store_true", help="Open template wizard")
-group.add_argument("--regenerate", action="store_true", help="Open regeneration wizard")
-args = parser.parse_args()
+        self.settings = Settings()
+        self.template_path = "test-files/template.md" #todo read from args
+    
+    def run(self):
+        args = self.parser.parse_args()
 
-if args.settings:
-    print("Not yet implemented.") #settings.cli()
-elif args.template:
-    print("Not yet implemented.") #template_gen.cli()
-elif args.regenerate:
-    print("Not yet implemented.") #readme_gen.regenerate()
-else:
-    generator = ReadmeGenerator(settings)
-    generator.cli("test-files/template.md")
+        if args.settings:
+            print("Not yet implemented.") #settings.cli()
+        elif args.template:
+            print("Not yet implemented.") #template_gen.cli()
+        elif args.regenerate:
+            print("Not yet implemented.") #readme_gen.regenerate()
+        else:
+            self.init_readme_generator()
+    
+    def init_readme_generator(self):
+        # Check for whether it can load settings and attempt to load them as they're required by the generator
+        
+        # First look at whether we have a template and settings to be able to run the readme generator
+        if os.path.exists(SETTINGS_LOCATION) and self.settings.load(SETTINGS_LOCATION):
+            generator = ReadmeGenerator(self.settings)
+            generator.cli(self.template_path)
+        else:
+            #Now we need to give options.
+            pass
+
+if __name__ == "__main__":
+    menu = MainMenu()
+    menu.run()
